4ae8d66c4b4506179d11249099e6339c13726901
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2022-06-21 22:35:21 +0100

    committed

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+
+
+document.getElementById("main");
 var name = 'Emmanuel Mkpurunchi F.';
 var height = '4.5ft';
 var country = 'Nigeria';
-document.write('Name: ' + name + '<br> Height: ' + height + ' <br> Country: ' + country);
+var Me = 'Name: ' + name + '<br> Height: ' + height + ' <br> Country: ' + country
+document.write(Me);
