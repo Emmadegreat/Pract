f8c82f60e80abcf50da5b2259a9c0e814ad7302b
Emma F. Mkpurunchi <87911811+Emmadegreat@users.noreply.github.com>
2022-06-22 17:45:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ document.getElementById("main");
 var name = 'Emmanuel Mkpurunchi F.';
 var height = '4.5ft';
 var country = 'Nigeria';
-<<<<<<< HEAD
-var Me = 'Name: ' + name + '<br> Height: ' + height + ' <br> Country: ' + country
+var Me = 'Name: ' + name + '<br> Height: ' + height + ' <br> Country: ' + country;
 document.write(Me);
 
-
-
-=======
-var Me = 'Name: ' + name + '.<br> Height: ' + height + '.<br> Country: ' + country;
-document.write(Me);
->>>>>>> 215c6acc2b91876240d074099640c5d924f052f5
